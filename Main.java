1939a618331952a78c8955a232c9aefdf60343b9
Orginys <116267884+Orginys@users.noreply.github.com>
2024-08-12 10:02:57 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll(); // удаляем минимальный элемент, чтобы осталось только k элементов
+        }
+
+        return minHeap.peek(); // возвращаем k-й по величине элемент
+    }
+}
